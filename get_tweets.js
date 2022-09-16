a4cee57cf6eb81f278dc7dd135836878bc9df0c5
tomoyahiroe <tomoya4creative@gmail.com>
2022-09-17 01:43:40 +0900

    add: length validation

--- get_tweets.js
+++ get_tweets.js
@@ -1,15 +1,16 @@
 import { Client } from "twitter-api-sdk";
 import process from "node:process";
 
-const userAccountId = "1475499209889824770"; // user account you wanna fetch its data
+const userAccountId = "1443494067799670792"; // user account you wanna fetch its data
 
 async function getTweets() {
   const client = new Client(process.env.TWITTER_BEARER_TOKEN);
   const response = await client.tweets.usersIdTweets(userAccountId, {
     max_results: 100,
     "tweet.fields": ["created_at", "public_metrics"],
-  });
+  })
   return response;
 }
 
 export { getTweets };
+
--- style_json.js
+++ style_json.js
@@ -1,18 +1,21 @@
 function styleJson(data) {
   const elements = [];
-  for (let i = 0; i < 100; i++) {
-    const element = data["data"][i];
+  let loopLength = data.data.length
+  if(data.data.length >= 100) loopLength = 100
+  for (let i = 0; i < data.data.length; i++) {
+    const element = data.data[i];
     elements[i] = {
-      "index": `${i}`,
-      "created_at": `${element.created_at}`,
-      "text": `${element.text}`,
-      "like_count": `${element.public_metrics.like_count}`,
-      "retweet_count": `${element.public_metrics.retweet_count}`,
-      "reply_count": `${element.public_metrics.reply_count}`,
-      "quote_count": `${element.public_metrics.quote_count}`,
+      index: i,
+      created_at: element.created_at,
+      text: element.text,
+      like_count: element.public_metrics.like_count,
+      retweet_count: element.public_metrics.retweet_count,
+      reply_count: element.public_metrics.reply_count,
+      quote_count: element.public_metrics.quote_count,
     };
+    elements[0] = element
   }
-  return JSON.stringify(elements,null,2);
+  return elements
 }
 
 export { styleJson };
