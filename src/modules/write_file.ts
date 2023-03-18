b783b0fc8c45ca93b48d8be620c4c2d4da9bac1e
tomoyahiroe <tomoya4creative@gmail.com>
2023-03-18 22:05:14 +0900

    fix: writeFile関数をTS化

--- src/modules/write_file.ts
+++ src/modules/write_file.ts
@@ -1,6 +1,6 @@
 import * as fs from "node:fs";
 
-async function writeFile(csv) {
+async function writeFile(csv: string) {
   return await fs.writeFile("result.csv", csv, (err) => {
     if (err) throw err;
     console.log("The file has been saved!");
